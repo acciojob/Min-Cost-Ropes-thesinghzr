8b2a8d4b9dd66dbb715d0b11b8053f19bd1a48bc
Suraj Singh <yousurajsingh@gmail.com>
2025-01-28 00:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function minCost(arr) {
+    let totalCost = 0;
+
+    // Continue until there is only one rope
+    while (arr.length > 1) {
+        arr.sort((a, b) => a - b);
+
+        // Pick the two smallest ropes
+        const first = arr.shift(); // Remove the smallest
+        const second = arr.shift(); // Remove the second smallest
+
+        const cost = first + second;
+        totalCost += cost;
+
+        arr.push(cost);
+    }
+
+    return totalCost;
 }
 
-module.exports=mincost;
+module.exports=minCost;
